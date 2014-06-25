b90fea7fdb436ab8f6d62559962f754e7e29c90f
Jon Eisenstein <jon@animoto.com>
2014-06-25 11:21:09 -0400

    Open files from tree

--- app/controllers/files.rb
+++ app/controllers/files.rb
@@ -1,10 +1,14 @@
 def file_tree(root)
   root_files = Dir["#{root}/*"]
   root_files.map do |f|
+    # Not working as a one-liner for some reason
+    r = /^(\.\/)(.*)$/
+    m = f.match(r)
+    stripped = $2
     if File.directory?(f)
-      [f, file_tree(f)]
+      [stripped, file_tree(f)]
     else
-      [f, nil]
+      [stripped, nil]
     end
   end
 end
@@ -14,4 +18,9 @@ end
 get "/files" do
   content_type :json
   {:project_files => file_tree(settings.project)}.to_json
+end
+
+get "/file/*" do
+  content_type :json
+  {:file_contents => File.read(params[:splat][0])}.to_json
 end
